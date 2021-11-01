fe3050f7b08f7de161aa52930d1e6eefbe4c9ee5
pextech <mcstain1639@gmail.com>
2021-11-01 21:45:55 +0200

    addedloaders

--- pages/_app.js
+++ pages/_app.js
@@ -8,35 +8,63 @@ import { GlobalProvider } from "../contexts/provider";
 import { NetworkIDs } from "@zoralabs/nft-hooks";
 import { MediaConfiguration } from "@zoralabs/nft-components";
 import { Web3ConfigProvider } from "@zoralabs/simple-wallet-provider";
+import { useEffect, useState } from 'react'
 
+import Loading from '../components/Loading'
 import { mediaConfigurationStyles } from "../styles/theme";
 import GlobalStyles from "../styles/GlobalStyles";
 import { Footer } from "../components/Footer";
 import { DAppProvider, ChainId } from "@usedapp/core";
 import NProgress from "nprogress";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 
-NProgress.configure({
-  minimum: 0.3,
-  easing: "ease",
-  speed: 800,
-  showSpinner: false,
-});
-
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-
-const config = {
-  readOnlyChainId: ChainId.Rinkeby,
-  readOnlyUrls: {
-    [ChainId.Rinkeby]: process.env.NEXT_PUBLIC_RPC_URL,
-  },
-};
 
 export default function CreateAuctionHouseApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  const [state, setState] = useState({
+    isRouteChanging: false,
+    loadingKey: 0,
+  })
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: true,
+        loadingKey: prevState.loadingKey ^ 1,
+      }))
+    }
+
+    const handleRouteChangeEnd = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: false,
+      }))
+    }
+
+    router.events.on('routeChangeStart', handleRouteChangeStart)
+    router.events.on('routeChangeComplete', handleRouteChangeEnd)
+    router.events.on('routeChangeError', handleRouteChangeEnd)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart)
+      router.events.off('routeChangeComplete', handleRouteChangeEnd)
+      router.events.off('routeChangeError', handleRouteChangeEnd)
+    }
+  }, [router.events])
+
+  const config = {
+    readOnlyChainId: ChainId.Rinkeby,
+    readOnlyUrls: {
+      [ChainId.Rinkeby]: process.env.NEXT_PUBLIC_RPC_URL,
+    },
+  };
+
   return (
     <>
+    <Loading isRouteChanging={state.isRouteChanging} key={state.loadingKey} />
       <GlobalStyles />
       <GlobalProvider>
         <DAppProvider config={config}>
